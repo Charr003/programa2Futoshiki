8b1c8761a84bc57dd4fa2f8a4a63e531585b3582
Charr003 <cargutierrez@estudiantec.cr>
2024-10-30 18:47:53 -0600

    Commit Main
    Primer commit

---
+++ programa2Futoshiki/src/main/java/com/mycompany/programa2futoshiki/Programa2Futoshiki.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.programa2futoshiki;
+
+/**
+ *
+ * @author XPC
+ */
+public class Programa2Futoshiki {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+    }
+}
